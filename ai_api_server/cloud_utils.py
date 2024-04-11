32c48763284a3f345763671957c8db22466f67d5
KYOOKIM <dev.kyoomin@gmail.com>
2024-04-12 02:23:50 +0900

    Fix gcs utils to list

--- ai_api_server/cloud_utils.py
+++ ai_api_server/cloud_utils.py
@@ -1,36 +1,56 @@
 import os
 import io
+from typing import List
 from PIL import Image
 from ai_api_server.logger import logger
-from ai_api_server.config import BUCKET_PREFIX, BUCKET_NAME, GCP_CREDENTIAL, GCS_URL_PREFIX
+from ai_api_server.config import BUCKET_PREFIX, BUCKET_NAME, FORMAT_DATE, GCP_CREDENTIAL, GCS_URL_PREFIX
 from google.cloud import storage
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = GCP_CREDENTIAL
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "test.json"
 
 
-def upload_image_to_gcs(image: Image.Image, dest_file_name: str) -> str:
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(BUCKET_NAME)
-    blob = bucket.blob(f"{BUCKET_PREFIX}/{dest_file_name}")
+def upload_image_to_gcs(images: List[Image.Image], dest_file_names: List[str]) -> bool:
+    try:
+        storage_client = storage.Client()
+        # bucket = storage_client.bucket(BUCKET_NAME)
+        bucket = storage_client.bucket("2024-profile")
+        
+        for image, dest_file_name in zip(images, dest_file_names):
+            # Convert the PIL Image to bytes
+            blob = bucket.blob(f"{BUCKET_PREFIX}/{dest_file_name}")
+            image_bytes = io.BytesIO()
+            image.save(image_bytes, format="PNG")
+            image_bytes.seek(0)
 
-    # Convert the PIL Image to bytes
-    image_bytes = io.BytesIO()
-    image.save(image_bytes, format="PNG")
-    image_bytes.seek(0)
+            # Upload the bytes to GCS
+            blob.upload_from_file(image_bytes, content_type="image/png")
+        print("Done")
+        return True
+    except Exception as e:
+        print("ee", e)
+        logger.error(f"Error-upload_gcs:{BUCKET_PREFIX}/{dest_file_names}::detail:{e}")
+        return False
 
-    # Upload the bytes to GCS
-    blob.upload_from_file(image_bytes, content_type="image/png")
-    logger.info(f"Success-upload_gcs-image_path:{BUCKET_PREFIX}/{dest_file_name}" )
-    return f"{BUCKET_PREFIX}/{dest_file_name}"
 
 
-def download_image_from_gcs(source_img_path) -> Image.Image:
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(BUCKET_NAME)
-    blob = bucket.blob(source_img_path)
+def download_image_from_gcs(source_img_paths: List[str]) -> tuple[bool, List[Image.Image]| None]:
+    try:
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(BUCKET_NAME)
+        result = []
+        for source_img_path in source_img_paths:
+            # Get the blob
+            blob = bucket.blob(source_img_path)
 
-    # Download the blob as bytes
-    image_bytes = blob.download_as_bytes()
+            # Download the blob as bytes
+            image_bytes = blob.download_as_bytes()
+            img = Image.open(io.BytesIO(image_bytes))
+            
+            # Create a PIL Image from the downloaded bytes
+            result.append(img)
+            
+        return True, result
+    except Exception as e:
+        logger.error(f"Error-download_gcs:{source_img_paths}::detail:{e}")
+        return False, None
 
-    # Create a PIL Image from the downloaded bytes
-    return Image.open(io.BytesIO(image_bytes))
